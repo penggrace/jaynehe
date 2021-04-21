040d6477a677ba273d53323dd02cf3de27b77812
penggrace <penggrace96@gmail.com>
2021-04-21 19:16:15 -0400

    updated bio

--- src/components/aboutBlurb.js
+++ src/components/aboutBlurb.js
@@ -4,8 +4,8 @@ import React from "react"
 const AboutBlurb = () => {
   return (
     <div className="about-blurb">
-      <div className="container">
-        <div className="inner-blurb">
+      <div className="inner-blurb">
+        <div className="container">
           <div className="content">
             <h3>PR</h3>
             <p>Jayne is a PR specialist who came from a multi-disciplinary background in social media, influencer marketing, and creative management. As one of the founding members of Outcast Studio, she broke into the industry while running the in-house press department for the emerging NYC designer Han Wen's eponymous womenswear brand. She then graduated to working as a communication consultant for the NYC cult favorite, Orseund Iris, specializing in Asian Markets.</p>
@@ -13,8 +13,8 @@ const AboutBlurb = () => {
             <p>As a NYC based producer, Jayne takes care of all aspects of Outcast production. She also focuses in overseeing artist management and production for Asia based clients. She continues to work on expanding the presence of Outcast Studio by seeking new business, brand, and talent collaboration opportunities within the U.S. and Asia.</p>
           </div>
         </div>
+        </div>
         <div className="fixed-misc">Producer + PR</div>
-      </div>
     </div>
   )
 }
